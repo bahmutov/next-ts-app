0c6578c117ea009e926570c82799a14499d3a139
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-09 16:01:53 -0500

    add comments

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// https://github.com/cypress-io/commit-info
 const {getBranch, getSha} = require('@cypress/commit-info')
 
 /** @type {import('next').NextConfig} */
@@ -5,6 +6,7 @@ const nextConfig = {
   reactStrictMode: true,
   generateBuildId: async () => {
     // make sure to use Vercel variables if available
+    // https://vercel.com/docs/concepts/projects/environment-variables
     const branch = process.env.VERCEL_GIT_COMMIT_REF || await getBranch() || 'unknown branch'
     const sha = process.env.VERCEL_GIT_COMMIT_SHA || await getSha() || 'unknown sha'
     const buildId = `${branch}:::${sha}`
