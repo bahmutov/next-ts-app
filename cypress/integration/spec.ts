26d950f35fdc86f8a3556bc6e2e0bf08fd167216
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-09 07:50:53 -0500

    no need for comment

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,7 +1,3 @@
-// enables intelligent code completion for Cypress commands
-// https://on.cypress.io/intelligent-code-completion
-/// <reference types="cypress" />
-
 it('loads the home page', () => {
   cy.visit('/')
 })
