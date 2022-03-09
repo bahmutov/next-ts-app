3edb251451280a0bb4559f8e276806262b772d27
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-09 15:45:54 -0500

    generate build id with the branch and commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const {getBranch, getSha} = require('@cypress/commit-info')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  generateBuildId: async () => {
+    const branch = await getBranch() || 'unknown branch'
+    const sha = await getSha() || 'unknown sha'
+    const buildId = `${branch}:::${sha}`
+    console.log('generated build id "%s"', buildId)
+    return buildId
+  }
 }
 
 module.exports = nextConfig
